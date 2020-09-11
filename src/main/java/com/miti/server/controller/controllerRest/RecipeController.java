6b97fbe5f7c3ae2c08d1ddca0816c0f70c2e7f58
Shveed <vania.shvedkov@yandex.ru>
2020-09-11 23:46:35 +0300

    changed get to post

--- src/main/java/com/miti/server/controller/controllerRest/RecipeController.java
+++ src/main/java/com/miti/server/controller/controllerRest/RecipeController.java
@@ -35,7 +35,7 @@ public class RecipeController {
         return recipeService.getRecipeById(id);
     }
 
-    @GetMapping("/getRecipesByCategory")
+    @PostMapping("/getRecipesByCategory")
     public List<Recipe> getRecipesByCategory(@RequestBody Category category) {
         return recipeService.getRecipesByCategory(category);
     }
