8e38e550109c3b8fe59539f18275521ffba6d370
OrizonJust <OrizonJust@gmail.com>
2020-09-15 14:02:28 +0300

    Открыл доступ для всех пользователей

--- src/main/java/com/miti/server/security/SecurityConfig.java
+++ src/main/java/com/miti/server/security/SecurityConfig.java
@@ -1,13 +1,41 @@
 package com.miti.server.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/*").permitAll();
+        http.
+                authorizeRequests()
+                .antMatchers("*").permitAll();
+                //.anyRequest().hasRole("ADMIN")
+//                .and()
+//                .formLogin()
+//                .loginPage("/login").permitAll().and()
+//                .logout().permitAll();
     }
+
+//    @Bean
+//    @Override
+//    public UserDetailsService userDetailsService() {
+//        UserDetails user = User.withDefaultPasswordEncoder()
+//                .username("user")
+//                .password("qwerty")
+//                .roles("USER")
+//                .build();
+//
+//        return new InMemoryUserDetailsManager(user);
+//    }
 }
