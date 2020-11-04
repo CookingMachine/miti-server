c90d35283996a0955da7d3530e257d6d77e39c33
OrizonJust <OrizonJust@gmail.com>
2020-11-04 20:56:24 +0300

    Создал контроллер MainPageController Также добавил условие для проверки авторизации на клиенте

---
+++ src/main/java/com/miti/server/controller/MainPageController.java
@@ -0,0 +1,39 @@
+package com.miti.server.controller;
+
+import com.miti.server.model.entity.Category;
+import com.miti.server.model.entity.Recipe;
+import com.miti.server.service.CategoryService;
+import com.miti.server.service.RecipeService;
+import com.sun.org.apache.xpath.internal.operations.String;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.persistence.Entity;
+import javax.persistence.EntityManager;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+public class MainPageController {
+
+  private final CategoryService categoryService;
+  private final RecipeService recipeService;
+
+  @GetMapping("/mainPage")
+  public void getMainPageElements(HttpServletResponse res) throws IOException {
+    res.setCharacterEncoding("UTF-8");
+    List<Category> categories = categoryService.getAllCategories();
+    List<Recipe> recipes = recipeService.getAllRecipes();
+    StringBuilder elements = new StringBuilder();
+    elements.append(categories.toString());
+    elements.append("\n");
+    elements.append(recipes.toString());
+    res.getWriter().println(elements);
+  }
+}
