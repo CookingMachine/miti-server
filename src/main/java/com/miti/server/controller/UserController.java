c22d978df0fb8c81b053ff5ab17ae8309dd8f736
OrizonJust <OrizonJust@gmail.com>
2021-04-24 21:33:55 +0300

    Изменил некоторые файлы для успешного деплоя сервера на хостинг

--- src/main/java/com/miti/server/controller/UserController.java
+++ src/main/java/com/miti/server/controller/UserController.java
@@ -14,7 +14,7 @@ import java.text.ParseException;
 import java.util.List;
 
 @RestController
-@RequestMapping(path = "api/v1/user")
+@RequestMapping(path = "/api/v1/user")
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class UserController {
 
